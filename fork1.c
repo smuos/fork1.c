0c0e75e60206b3ae8a4910a4323a73c70f8bba74
Joel Kuntz <joel.kuntz.92@gmail.com>
2014-09-16 16:33:05 -0300

    Fix format and output.

--- fork1.c
+++ fork1.c
@@ -20,14 +20,13 @@ main(int argc, char* argv[])
         fprintf(stdout, "OS too hard, could not cut.\n");
         exit(0);
     } else if (rc == 0) {
-        fprintf(stderr, "Child can't talk to strangers. But doesn't listen anyhow.\n"); 
-        //exit(1); 
+        fprintf(stderr, "Child can't talk to strangers. But doesn't listen anyhow.\n");  
         printf("Hello, I am child (pid:%d)\n", (int)getpid()); 
         sleep(1);
     } else if (rc > 0) {
         int wc = wait(NULL); //is child finished?
-        printf("Please leave my child alone, I am %d (wc:%d) (pid:%d)\n",
-           getpid(), wc, (int) rc);
+        printf("Please leave my child (pid:%d) alone, I am its guardian (pid:%d)\n",
+            wc, getpid());
     }
     return SUCCESS;
 }
