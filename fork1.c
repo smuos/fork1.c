aa92fcc9dc55f2e78fe8f0d59838dee6a24599e4
Glavin Wiechert <glavin.wiechert@gmail.com>
2014-09-16 16:12:29 -0300

    Fixes #5. Change function knife to fork.

--- fork1.c
+++ fork1.c
@@ -14,7 +14,7 @@ main(int argc, const char** argv)
     }
 
     printf("Hi stranger! I'm (pid:%d)\n", (int) getpid());
-    int rc = knife(); //slice off another process
+    int rc = fork(); //slice off another process
     if (rc < -1) {
         // Could not cut another process
         fprintf(stdout, "OS too hard, could not cut.\n");
