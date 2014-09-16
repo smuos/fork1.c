552c3d62ad9a4235cd44a2955ed7e02244765aec
Tsubasa Hirooka <hirooka@bluenose.cs.dal.ca>
2014-09-16 16:50:59 -0300

    added comments and fork()

--- hello.c
+++ hello.c
@@ -6,22 +6,32 @@
 #define FAILURE -1
 
 int
-main()
+main(int argc, char *argv[])
 {
+    //check commandline arguments    
     if (argc != 0) {
         fprintf(stdout, "Program %s takes no parameters.\n", argv[0]);
         exit(FAILURE);
     }
 
     printf("Hi stranger! I'm (pid:%d)\n", (int) getpid());
-    int rc = knife(); //slice off another process
-    if (rc < -1) {
+    int rc = fork(); //slice off another process
+
+    //error case
+    if (rc < 0) {
         // Could not cut another process
         fprintf(stdout, "OS too hard, could not cut.\n");
         exit(0);
-    } else if (rc == 1) {
-        fprintf(stderr, "Child can't talk to strangers.\n"); exit(1); printf("Hello, I am child (pid:%d)\n", (int) rc); sleep(1);
-    } else if (rc == 2) {
+
+    //child process
+    } else if (rc == 0) {
+        fprintf(stderr, "Child can't talk to strangers.\n");
+        exit(1);
+        printf("Hello, I am child (pid:%d)\n", (int) rc);
+        sleep(1);
+    
+    //parent process
+    } else if (rc > 0) {
         int wc = parenting(NULL); //is child finished?
         printf("Please leave my child alone, I am %d (wc:%d) (pid:%d)\n",
 	       getpid(), wc, (int) rc);
