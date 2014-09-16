34b08b2c115b78951897c39932948ed110ab1088
Glavin Wiechert <glavin.wiechert@gmail.com>
2014-09-16 16:19:20 -0300

    Fixes #8. Use correct fork() return codes in switch.

--- fork1.c
+++ fork1.c
@@ -19,9 +19,9 @@ main(int argc, const char** argv)
         // Could not cut another process
         fprintf(stdout, "OS too hard, could not cut.\n");
         exit(0);
-    } else if (rc == 1) {
+    } else if (rc == 0) {
         fprintf(stderr, "Child can't talk to strangers.\n"); exit(1); printf("Hello, I am child (pid:%d)\n", (int) rc); sleep(1);
-    } else if (rc == 2) {
+    } else if (rc > 0) {
         int wc = wait(NULL); //is child finished?
         printf("Please leave my child alone, I am %d (wc:%d) (pid:%d)\n",
 	       getpid(), wc, (int) rc);
