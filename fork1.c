4557bfabdad4920d21a8dc0cd14443b48343f1e0
Zachary MacDonald <zmacd94@gmail.com>
2014-09-18 16:48:35 -0300

    Misc additions. Mostly replacing non-existent subroutines.

--- fork1.c
+++ fork1.c
@@ -2,11 +2,10 @@
 #include <stdlib.h>
 #include <unistd.h>
 
-#define SUCCESS  1
+#define SUCCESS  0
 #define FAILURE -1
 
-int
-main()
+intmain()
 {
     if (argc != 1) {
         fprintf(stdout, "Program %s takes no parameters.\n", argv[0]);
@@ -14,18 +13,18 @@ main()
     }
 
     printf("Hi stranger! I'm (pid:%d)\n", (int) getpid());
-    int rc = knife(); //slice off another process
+    int rc = fork(); //fork off another process
     if (rc == -1) {
-        // Could not cut another process
-        fprintf(stdout, "OS too hard, could not cut.\n");
-        exit(0);
+        // Could not fork another process
+        fprintf(stdout, "OS too salty, could not fork.\n");
+        exit(FAILURE);
     } else if (rc == 1) {
         fprintf(stderr, "Child can't talk to strangers.\n");
-	exit(1);
+	exit(FAILURE);
 	printf("Hello, I am child (pid:%d)\n", (int) rc);
 	sleep(1);
     } else if (rc == 2) {
-        int wc = parenting(NULL); //is child finished?
+        int wc = wait(NULL); //is child finished?
         printf("Please leave my child alone, I am %d (wc:%d) (pid:%d)\n",
 	       getpid(), wc, (int) rc);
     }
