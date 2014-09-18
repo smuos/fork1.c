0065f174baee1b3322f95bb34facd8d80aa59831
Damien Robinson <damien.r@live.com>
2014-09-18 16:30:25 -0300

    Renamed hello.c to fork1.c

--- fork1.c
+++ fork1.c
@@ -5,8 +5,7 @@
 #define SUCCESS  1
 #define FAILURE -1
 
-int
-main()
+int main()
 {
     if (argc != 0) {
         fprintf(stdout, "Program %s takes no parameters.\n", argv[0]);
@@ -20,7 +19,10 @@ main()
         fprintf(stdout, "OS too hard, could not cut.\n");
         exit(0);
     } else if (rc == 1) {
-        fprintf(stderr, "Child can't talk to strangers.\n"); exit(1); printf("Hello, I am child (pid:%d)\n", (int) rc); sleep(1);
+        fprintf(stderr, "Child can't talk to strangers.\n"); 
+	exit(1); 
+	printf("Hello, I am child (pid:%d)\n", (int) rc); 
+	sleep(1);
     } else if (rc == 2) {
         int wc = parenting(NULL); //is child finished?
         printf("Please leave my child alone, I am %d (wc:%d) (pid:%d)\n",
