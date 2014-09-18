f9a220422274f3f88cc59d372bb29010ba196d7d
Kashif <kashiv.agnelite@gmail.com>
2014-09-18 17:17:32 -0300

    Corrected the rc(fork) conditions

--- fork1.c
+++ fork1.c
@@ -1,3 +1,4 @@
+//include files
 #include <stdio.h>
 #include <stdlib.h>
 #include <unistd.h>
@@ -5,10 +6,9 @@
 #define SUCCESS  1
 #define FAILURE -1
 
-int
-main(int argc, char* argv [])
+int main(int argc, char* argv [])
 {
-    if (argc != 0) {
+    if (argc != 1) { //if there is no command line parameter
         fprintf(stdout, "Program %s takes no parameters.\n", argv[0]);
         exit(FAILURE);
     }
@@ -19,9 +19,9 @@ main(int argc, char* argv [])
         // Could not cut another process
         fprintf(stdout, "OS too hard, could not cut.\n");
         exit(0);
-    } else if (rc == 1) {
+    } else if (rc == 0) {
         fprintf(stderr, "Child can't talk to strangers.\n"); exit(1); printf("Hello, I am child (pid:%d)\n", (int) rc); sleep(1);
-    } else if (rc == 2) {
+    } else if (rc < 1) {
         int wc = wait(NULL); //is child finished?
         printf("Please leave my child alone, I am %d (wc:%d) (pid:%d)\n",
 	       getpid(), wc, (int) rc);
