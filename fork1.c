ec1a8371d47e7c355240f6ea74e597081c734828
JINGJING TANG <aaron.jj@hotmail.com>
2014-09-18 17:57:23 -0300

    Print out child id

--- fork1.c
+++ fork1.c
@@ -22,7 +22,7 @@ main(int argc, char *argv[])
     } else if (rc == 0) {
         fprintf(stderr, "Child can't talk to strangers.\n");
 	exit(1);
-	printf("Hello, I am child (pid:%d)\n", (int) rc);
+	printf("Hello, I am child (pid:%d)\n", (int) getpid());
 	sleep(1);
     } else if (rc > 0) {
         int wc = wait(NULL); //is child finished?
