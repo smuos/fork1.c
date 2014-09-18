780423e610dc827f46106c356191cb537a85df2e
Matthew Musoke <msk.matt2@gmail.com>
2014-09-18 18:13:55 -0300

    Removed child cant talk to strangers and exit after it. Fixed some readability

--- fork1.c
+++ fork1.c
@@ -19,9 +19,12 @@ main(int argc, char *argv[])
         // Could not cut another process
         fprintf(stdout, "OS too hard, could not cut.\n");
         exit(0);
-    } else if (rc == 1) {
-        fprintf(stderr, "Child can't talk to strangers.\n"); exit(1); printf("Hello, I am child (pid:%d)\n", (int) rc); sleep(1);
-    } else if (rc == 2) {
+    }
+    else if (rc == 1) {
+	printf("Hello, I am child (pid:%d)\n", (int) rc);
+	sleep(1);
+    }
+    else if (rc == 2) {
         int wc = parenting(NULL); //is child finished?
         printf("Please leave my child alone, I am %d (wc:%d) (pid:%d)\n",
 	       getpid(), wc, (int) rc);
