39e8a54e460b120bee9ad0861b37952cde803c36
Xujie Zheng <xj721386@dal.ca>
2014-09-19 01:09:29 -0300

    edited the argc compare, if it is equal to 0 no argument is passing to the program

--- fork1.c
+++ fork1.c
@@ -7,29 +7,27 @@
 int
 main(int argc, char *argv[])
 {
-    if (argc != 0) {
+    if (argc == 0) {//if argc is equal to zero indicates that there is no argument
         fprintf(stdout, "Program %s takes no parameters.\n", argv[0]);
         exit(FAILURE);
     }
 
     printf("Hi stranger! I'm (pid:%d)\n", (int) getpid());
     int rc = fork(); //slice off another process by fork
-    
     if (rc < 0) {//less than 0,or equal -1
         // Could not cut another process
 
         //fprintf(stdout, "OS too hard, could not cut.\n");
-	printf("OS too hard, could not cut.\n");//stdout is the screen and a 
-						//simple printf can do the job
+	printf("OS too hard, could not cut.\n");//stdout is the screen and a simple printf can do the job
 
 	//unable to fork output to error file
 	fprintf(stderr, "fail to fork\n");
         exit(0);//or exit(SUCCESS) for consistance
 
     } else if (rc == 0) {//this is children
-        fprintf(stderr, "Child can't talk to strangers.\n"); 
-	exit(1); 
-	printf("Hello, I am child (pid:%d)\n", (int) rc); 
+        fprintf(stderr, "Child can't talk to strangers.\n");
+	exit(1);
+	printf("Hello, I am child (pid:%d)\n", (int) rc);
 	sleep(1);
     } else if (rc > 0) {
         int wc = wait(NULL); //is child finished?
