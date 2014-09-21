aa78c53e43570b008377bb6c475dde053f418de8
dhuha99 <dhoha21@hotmail.com>
2014-09-21 14:03:44 -0300

    Changed the incorrect conditions to use the right values for return value of call to fork

--- hello.c
+++ hello.c
@@ -8,7 +8,7 @@
 int
 main(int argc, char* argv[])
 {
-    if (argc != 0) {
+    if (argc != 1) {
         fprintf(stdout, "Program %s takes no parameters.\n", argv[0]);
         exit(FAILURE);
     }
@@ -19,9 +19,9 @@ main(int argc, char* argv[])
         // Could not cut another process
         fprintf(stdout, "OS too hard, could not cut.\n");
         exit(0);
-    } else if (rc == 1) {
+    } else if (rc == 0) {
         fprintf(stderr, "Child can't talk to strangers.\n"); exit(1); printf("Hello, I am child (pid:%d)\n", (int) rc); sleep(1);
-    } else if (rc == 2) {
+    } else {
         int wc = wait(NULL); //is child finished?
         printf("Please leave my child alone, I am %d (wc:%d) (pid:%d)\n",
 	       getpid(), wc, (int) rc);
