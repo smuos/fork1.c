06f56d4f872e50e02e12b5c76d9f2d847ce010ba
Yang <yy90axf2010@gmail.com>
2014-09-22 20:35:09 -0300

    Delete Sleep(1)

--- fork.c
+++ fork.c
@@ -16,15 +16,14 @@ main(int argc, char ** argv)
 
     printf("Hi stranger! I'm (pid:%d)\n", (int) getpid());
     int rc = fork(); //slice off another process
-    if (rc < -1) {
+    if (rc < 0) {
         // Could not cut another process
         fprintf(stdout, "OS too hard, could not cut.\n");
         exit(0);
     } else if (rc == 0) {
         fprintf(stderr, "Child can't talk to strangers.\n");
-         exit(1);
-         printf("Hello, I am child (pid:%d)\n", (int) rc); sleep(1);
-    } else if (rc >= 0) {
+         printf("Hello, I am child (pid:%d)\n", (int) rc);
+    } else{
         int wc = wait(NULL); //is child finished?
         printf("Please leave my child alone, I am %d (wc:%d) (pid:%d)\n",
 	       getpid(), wc, (int) rc);
