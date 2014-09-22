0f4c46dfbd5aa83aecd3daeb6a6faba1cfb9630e
JieZhang0918 <jz314907@dal.ca>
2014-09-22 19:21:32 -0300

    assignment1v1

--- hello.c
+++ hello.c
@@ -2,29 +2,28 @@
 #include <stdlib.h>
 #include <unistd.h>
 
-#define SUCCESS  1
-#define FAILURE -1
 
 int
-main()
+main(int argc, char *argv[])
 {
-    if (argc != 0) {
+    if (argc != 1) {
         fprintf(stdout, "Program %s takes no parameters.\n", argv[0]);
-        exit(FAILURE);
+        exit(1);//exit if the argument not equal to 1,
     }
-
+    
     printf("Hi stranger! I'm (pid:%d)\n", (int) getpid());
-    int rc = knife(); //slice off another process
-    if (rc < -1) {
+    int rc = fork(); //slice off another process
+    if (rc <0) {//if rc<0 exit the program
         // Could not cut another process
         fprintf(stdout, "OS too hard, could not cut.\n");
-        exit(0);
-    } else if (rc == 1) {
-        fprintf(stderr, "Child can't talk to strangers.\n"); exit(1); printf("Hello, I am child (pid:%d)\n", (int) rc); sleep(1);
-    } else if (rc == 2) {
-        int wc = parenting(NULL); //is child finished?
+        exit(1);
+    } else if (rc ==0) {//child(new procoess from fork())
+        fprintf(stderr, "Child can't talk to strangers.\n"); printf("Hello, I am child (pid:%d)\n", (int)getpid()); sleep(1);
+    } else {
+        int wc = wait(NULL); //is child finished?
         printf("Please leave my child alone, I am %d (wc:%d) (pid:%d)\n",
-	       getpid(), wc, (int) rc);
+               rc,wc, (int) getpid());
     }
-    return SUCCESS;
+    return 0;
 }
+
