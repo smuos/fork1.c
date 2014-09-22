ffcacb8bc0d524d8c4e92e8ce7ccafd1072feb75
ZhenqiangLuo <manyutingfeng@hotmail.com>
2014-09-22 02:25:27 -0300

    stdbool.h included.

--- hello.c
+++ hello.c
@@ -1,6 +1,7 @@
 #include <stdio.h>
 #include <stdlib.h>
 #include <unistd.h>
+#include <stdbool.h>
 
 #define SUCCESS  1
 #define FAILURE -1
@@ -8,23 +9,23 @@
 int
 main()
 {
-    if (argc != 0) {
-        fprintf(stdout, "Program %s takes no parameters.\n", argv[0]);
-        exit(FAILURE);
-    }
+  if (argc != 0) {
+    fprintf(stdout, "Program %s takes no parameters.\n", argv[0]);
+    exit(FAILURE);
+  }
 
-    printf("Comments Hi stranger! I'm (pid:%d)\n", (int) getpid());
-    int rc = knife(); //slice off another process
-    if (rc < -1) {
-        // Could not cut another process
-        fprintf(stdout, "OS too hard, could not cut.\n");
-        exit(0);
-    } else if (rc == 1) {
-        fprintf(stderr, "Child can't talk to strangers.\n"); exit(1); printf("Hello, I am child (pid:%d)\n", (int) rc); sleep(1);
-    } else if (rc == 2) {
-        int wc = parenting(NULL); //is child finished?
-        printf("Please leave my child alone, I am %d (wc:%d) (pid:%d)\n",
-	       getpid(), wc, (int) rc);
-    }
-    return SUCCESS;
+  printf("Hi stranger! I'm (pid:%d)\n", (int) getpid());
+  int rc = knife(); //slice off another process
+  if (rc < -1) {
+    // Could not cut another process
+    fprintf(stdout, "OS too hard, could not cut.\n");
+    exit(0);
+  } else if (rc == 1) {
+    fprintf(stderr, "Child can't talk to strangers.\n"); exit(1); printf("Hello, I am child (pid:%d)\n", (int) rc); sleep(1);
+  } else if (rc == 2) {
+    int wc = parenting(NULL); //is child finished?
+    printf("Please leave my child alone, I am %d (wc:%d) (pid:%d)\n",
+	   getpid(), wc, (int) rc);
+  }
+  return SUCCESS;
 }
