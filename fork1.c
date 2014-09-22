9f125d0c0e05e45453b0605de85c8f09398835c9
jcsl94 <jcslucas94@hotmail.com>
2014-09-22 15:02:42 -0300

    I think something is messing up in my commits so this is a test, fixed some of the bugs and code format.

--- fork1.c
+++ fork1.c
@@ -7,23 +7,21 @@
 
 int main(int argc, char* argv[])
 {
-    if (argc != 0) {
+    if (argc != 1) {
         fprintf(stdout, "Program %s takes no parameters.\n", argv[0]);
         exit(FAILURE);
     }
 	
     printf("Hi stranger! I'm (pid:%d)\n", (int) getpid());
     int rc = fork(); //slice off another process
-    if (rc < -1) {
+    if (rc == -1) {
         // Could not cut another process
         fprintf(stdout, "OS too hard, could not cut.\n");
         exit(0);
-    } else if (rc == 1) {
-        fprintf(stderr, "Child can't talk to strangers.\n");
-	exit(1); 
+    } else if (rc == 0) { 
 	printf("Hello, I am child (pid:%d)\n", (int) rc);
 	sleep(1);
-    } else if (rc == 2) {
+    } else if (rc > 0) {
         int wc = wait(NULL); //is child finished?
         printf("Please leave my child alone, I am %d (wc:%d) (pid:%d)\n",
 	       getpid(), wc, (int) rc);
