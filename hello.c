ebf09b359803aec777ef8f5aaddfdf3992961af4
Yiying Zhang <yiying@bluenose.cs.dal.ca>
2014-09-23 12:07:56 -0300

    change the value

--- hello.c
+++ hello.c
@@ -19,7 +19,7 @@ main()
         // Could not cut another process
         fprintf(stdout, "OS too hard, could not cut.\n");
         exit(0);
-    } else if (rc == 1) {
+    } else if (rc == 0) //check if the pid is equal to 0 {
         fprintf(stderr, "Child can't talk to strangers.\n"); 
 	exit(1); 
 	printf("Hello, I am child (pid:%d)\n", (int) rc); 
