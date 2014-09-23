769387c43b8798b943592dbcfb274a7433680089
LYC1929 <lingyuan1929@hotmail.com>
2014-09-23 12:35:13 -0300

    Change rc == 1 to rc == 0

--- fork1.c
+++ fork1.c
@@ -21,7 +21,7 @@ main(int argc, char *argv[])
         // Could not cut another process
         fprintf(stdout, "OS too hard, could not cut.\n");
         exit(0);
-    } else if (rc == 1) {
+    } else if (rc == 0) {
         fprintf(stderr, "Child can't talk to strangers.\n");
         exit(1);
         printf("Hello, I am child (pid:%d)\n", (int) getpid());
