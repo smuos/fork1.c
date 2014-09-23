237f0c2fae9442f3abd9bde0addac70ddc58fcce
Priom Chowdhury <priom.galaxy@gmail.com>
2014-09-23 13:55:48 -0700

    Lab 2 submission by Nrimoni Chowdhury

--- hello.c
+++ hello.c
@@ -6,7 +6,7 @@
 #define FAILURE -1
 
 int
-main()
+main(int argc, char *argv[])
 {
     if (argc != 0) {
         fprintf(stdout, "Program %s takes no parameters.\n", argv[0]);
@@ -14,15 +14,16 @@ main()
     }
 
     printf("Hi stranger! I'm (pid:%d)\n", (int) getpid());
-    int rc = knife(); //slice off another process
+    int rc = fork(); //slice off another process
     if (rc < -1) {
         // Could not cut another process
         fprintf(stdout, "OS too hard, could not cut.\n");
         exit(0);
     } else if (rc == 1) {
-        fprintf(stderr, "Child can't talk to strangers.\n"); exit(1); printf("Hello, I am child (pid:%d)\n", (int) rc); sleep(1);
+        fprintf(stderr, "Child can't talk to strangers.\n"); exit(1); 
+    	printf("Hello, I am child (pid:%d)\n", (int) rc); sleep(1);
     } else if (rc == 2) {
-        int wc = parenting(NULL); //is child finished?
+        int wc = wait(); //is child finished?
         printf("Please leave my child alone, I am %d (wc:%d) (pid:%d)\n",
 	       getpid(), wc, (int) rc);
     }
