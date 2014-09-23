33ca3683d176f90e8342b371ceea02e31a514fee
Kashif <kashiv.agnelite@gmail.com>
2014-09-22 21:37:45 -0300

    Modified the rc conditions (again)

--- fork1.c
+++ fork1.c
@@ -15,13 +15,12 @@ int main(int argc, char* argv [])
 
     printf("Hi stranger! I'm (pid:%d)\n", (int) getpid());
     int rc = fork(); //slice off another process
-    if (rc < -1) {
+    if (rc < -0) {
         // Could not cut another process
         fprintf(stdout, "OS too hard, could not cut.\n");
         exit(0);
     } else if (rc == 0) {
         fprintf(stderr, "Child can't talk to strangers.\n");
-        exit(1);
         printf("Hello, I am child (pid:%d)\n", (int) getpid()); 
         sleep(1);
     } else if (rc > 0) {
