b70a6038560cc09f1cc20c7b2d033f39084f5b5e
Yiying Zhang <yiying@bluenose.cs.dal.ca>
2014-09-23 12:10:43 -0300

    correct the mistake happen in last time and change the value from rc to the function getpid()

--- hello.c
+++ hello.c
@@ -19,10 +19,11 @@ main()
         // Could not cut another process
         fprintf(stdout, "OS too hard, could not cut.\n");
         exit(0);
-    } else if (rc == 0) //check if the pid is equal to 0 {
+    } else if (rc == 0){ 
+      //check if the pid is equal to 0
         fprintf(stderr, "Child can't talk to strangers.\n"); 
 	exit(1); 
-	printf("Hello, I am child (pid:%d)\n", (int) rc); 
+	printf("Hello, I am child (pid:%d)\n", (int) getpid()); 
 	sleep(1);
     } else if (rc == 2) {
         int wc = wait(NULL); //wait function
