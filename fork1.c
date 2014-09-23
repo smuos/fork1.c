c4b01f02e7a495f6d3f2ead68d285187c240ac50
LYC1929 <lingyuan1929@hotmail.com>
2014-09-23 13:22:32 -0300

    Add some comments

--- fork1.c
+++ fork1.c
@@ -9,29 +9,31 @@
 int
 main(int argc, char *argv[])
 {
+    // If the program takes any paraments, print out a message
     if (argc != 1) {
         fprintf(stdout, "Program %s takes no parameters.\n", argv[0]);
-        exit(FAILURE);
+        exit(FAILURE);  // exit the program (failure)
     }
 
     printf("Hi stranger! I'm (pid:%d)\n", (int) getpid());
     
-    int rc = fork(); //slice off another process
+    int rc = fork();  // slice off another process
     
+    // If fork failed, exit the program (failure)
     if (rc < 0) {
         // Could not cut another process
         fprintf(stdout, "OS too hard, could not cut.\n");
         exit(0);
-    } else if (rc == 0) {
+    } else if (rc == 0) {  // when rc is child
         fprintf(stderr, "Child can't talk to strangers.\n");
-        exit(1);
+        exit(1);  // exit (success)
         printf("Hello, I am child (pid:%d)\n", (int) getpid());
-        sleep(1);
-    } else {
-        int wc = wait(NULL); //is child finished?
+        sleep(1);  // sleep for 1 second
+    } else {  // when rc is parent
+        int wc = wait(NULL);  //wait for child finished
         printf("Please leave my child alone, I am %d (wc:%d) (pid:%d)\n",
 	       (int) getpid(), wc, (int) rc);
     }
     
-    return SUCCESS;
+    return SUCCESS;  // run successfully
 }
