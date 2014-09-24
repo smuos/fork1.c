d10e350e9fa96c121b83a3b295f593b7ed30e344
Matthew Smith <mpsmith414@gmail.com>
2014-09-24 12:49:48 -0300

    fixed the final else branch

--- fork.c
+++ fork.c
@@ -8,19 +8,19 @@
 int
 main(int argc, char *argv[])
 {
-    if (argc != 0) {
+    if (argc >= 1) {
         fprintf(stdout, "Program %s takes no parameters.\n", argv[0]);
         exit(FAILURE);
     }
    
     printf("Hi stranger! I'm (pid:%d)\n", (int) getpid());
     int rc = fork(); //slice off another process
-    if (rc < -1) {
-        // Could not cut another process
-        fprintf(stdout, "OS too hard, could not cut.\n");
+    if (rc < 0) {
+        // fork failed; exit
+        fprintf(stderr, "fork failed\n");
         exit(0);
-    } else if (rc == 1) {
-        fprintf(stderr, "Child can't talk to strangers.\n"); exit(1); printf("Hello, I am child (pid:%d)\n", (int) rc); sleep(1);
+    } else if (rc == 0) { // child (new process)
+        printf("Hello, I am child (pid:%d)\n", (int) getpid());
     } else if (rc == 2) {
         int wc = wait(NULL); //is child finished?
         printf("Please leave my child alone, I am %d (wc:%d) (pid:%d)\n",
