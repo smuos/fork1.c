4258cbf53db2dd963035eed9ba0b33c63387e8f4
Xujie <xj721386@dal.ca>
2014-09-25 23:43:53 +0000

    tested in the smu server and fixed a bug for the wait

--- fork1.c
+++ fork1.c
@@ -1,6 +1,8 @@
 #include <stdio.h>
 #include <stdlib.h>
 #include <unistd.h>
+#include <sys/wait.h>
+
 #define SUCCESS  0 //this two declration is used for the exit command
 #define FAILURE  1 //and exit(0) stands for normal exit, no error VICE VERSA
 
@@ -18,9 +20,8 @@ int main(int argc, char *argv[])
     if (rc < 0) {//less than 0,or equal -1
         // Could not cut another process
 
-        fprintf(stderr, "OS too hard, could not cut.\n");//the error is printing to the err log  not the screen
-	printf("OS too hard, could not cut.\n");//stdout is the screen and a simple printf can do the job
-
+        fprintf(stderr, "failled to fork\n");//the error is printing to the err log  not the screen
+	fprintf(stdout,"OS too hard, could not cut.\n");
         exit(FAILURE);//exit(1) indicates error occurs
 
     } else if (rc == 0) {//this is children
@@ -30,14 +31,14 @@ int main(int argc, char *argv[])
 	if the purpose of the above two line is to modify the process to do some other tasks, a exec() should be call
 	instead of exit()
 */
-	printf("Hello, I am child (pid:%d)\n", (int) rc);
+	fprintf(stdout,"Hello, I am child (pid:%d)\n", (int) rc);
 	sleep(1);//wait for one second
 
     } else if (rc > 0) {
         int wc = wait(NULL); //check if child finished?
 				 //the correct command is wait instead of parenting
 				 //although it makes more sense
-        printf("Please leave my child alone, I am %d (wc:%d) and my child pid is%d\n",
+        fprintf(stdout,"Please leave my child alone, I am %d (wc:%d) and my child pid is%d\n",
 	       getpid(), wc, (int) rc);
     }
     return SUCCESS;// return 0 normal exit
